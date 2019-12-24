473fda87135fbb4e5b90cea9bd7d1e063ccb886c
devneutronco <44727433+devneutronco@users.noreply.github.com>
2019-12-24 08:39:38 -0800

    Add files via upload

---
+++ boards/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Board
+
+
+def home(request):
+    boards = Board.objects.all()
+    return render(request, 'home.html', {'boards': boards})
+
+def board_topics(request, pk):
+        board = Board.objects.get(pk=pk)
+        return render(request, 'topics.html', {'board': board})
